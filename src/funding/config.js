9d3c72c753394fe569f70f1145143379b4edcd67
RahulKannan <rahulkannan144@gmail.com>
2022-08-30 17:19:35 +0530

    Space refractor.

--- src/funding/config.js
+++ src/funding/config.js
@@ -62,7 +62,7 @@ export function getFundingConfig() : { [$Values<typeof FUNDING>] : ?FundingSourc
             [ FUNDING.MAXIMA ]:         (!__TREE_SHAKE__ || (typeof __FUNDING_ELIGIBILITY__.maxima !== 'undefined'         && __FUNDING_ELIGIBILITY__.maxima.eligible)) ? getMaximaConfig() : null,
             [ FUNDING.MERCADOPAGO ]:    (!__TREE_SHAKE__ || (typeof __FUNDING_ELIGIBILITY__.mercadopago !== 'undefined'    && __FUNDING_ELIGIBILITY__.mercadopago.eligible)) ? getMercadopagoConfig() : null,
             [ FUNDING.MULTIBANCO ]:     (!__TREE_SHAKE__ || (typeof __FUNDING_ELIGIBILITY__.multibanco !== 'undefined'     && __FUNDING_ELIGIBILITY__.multibanco.eligible)) ? getMultibancoConfig() : null,
-            [ FUNDING.PAIDY ]:     (!__TREE_SHAKE__ || (typeof __FUNDING_ELIGIBILITY__.paidy !== 'undefined'     && __FUNDING_ELIGIBILITY__.paidy.eligible)) ? getPaidyConfig() : null
+            [ FUNDING.PAIDY ]:          (!__TREE_SHAKE__ || (typeof __FUNDING_ELIGIBILITY__.paidy !== 'undefined'          && __FUNDING_ELIGIBILITY__.paidy.eligible)) ? getPaidyConfig() : null
         };
     });
 }
